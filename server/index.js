a39bab793dc733a3e1f888ca08c4f615c96a1f3b
Reed Bruskin <reedbruskin@google.com>
2021-03-08 20:27:54 -0800

    Uncommenting other APIs to render all services

--- server/index.js
+++ server/index.js
@@ -6,9 +6,9 @@ const port = 3000;
 const axios = require('axios').default;
 
 const shoppingIP = '13.52.16.25:3004';
-// const sellerIP = '3.21.248.149:3005';
-// const reviewsIP = '54.151.123.24:3002';
-// const imagesIP = '13.52.213.118:3006';
+const sellerIP = '18.221.203.49:3005';
+const reviewsIP = '54.176.185.40:3002';
+const imagesIP = '3.101.55.156:3006';
 
 app.use(compression());
 app.use('/items/:itemId', express.static('client'));
@@ -18,9 +18,7 @@ app.use('/items/:itemId', express.static('client'));
 // #############################
 
 // Shopping Service Amazon EC2 Instance
-// http://18.222.223.190:3004/items/1
 app.get('/shopping/', (req, res) => {
-  // axios.get('https://fetsy-shopping.s3-us-west-1.amazonaws.com/bundle.js')
   let itemID = req.params.itemId;
   // axios.get(`http://localhost:3004/bundle.js`)
   axios.get(`http://${shoppingIP}/bundle.js`)
@@ -31,6 +29,47 @@ app.get('/shopping/', (req, res) => {
       res.status(404).send(`Bundle retrieval error: ${error}`);
     });
 });
+
+// Seller Service Amazon EC2 Instance
+app.get('/seller', (req, res) => {
+  console.log('hitting seller endpoint');
+  axios.get(`http://${sellerIP}/bundle.js`)
+  // axios.get('http://localhost:3005')
+    .then(function (response) {
+      res.send(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+// Reviews Service Amazon EC2 Instance
+app.get('/reviews', (req, res) => {
+  console.log('hitting seller endpoint');
+  axios.get(`http://${reviewsIP}/bundle.js`)
+    .then(function (response) {
+      res.send(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+// Item Images Service Amazon EC2 Instance
+app.get('/images', (req, res) => {
+  console.log('hitting images endpoint');
+  axios.get(`http://${imagesIP}/bundle.js`)
+    .then(function (response) {
+      res.send(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+// ##################################################
+// Rerouting requests from services to their correct ports if necessary
+// ##################################################
 
 // Shopping Service
 app.get('/shopping/items/:itemId', (req, res) => {
@@ -46,67 +85,6 @@ app.get('/shopping/items/:itemId', (req, res) => {
     });
 });
 
-// // Seller Service Amazon EC2 Instance
-// // http://3.21.248.149:3005/items/2/
-// app.get('/seller', (req, res) => {
-//   console.log('hitting seller endpoint');
-//   // axios.get(`http://${sellerIP}/items/1/bundle.js`)
-//   axios.get('http://localhost:3005')
-//     .then(function (response) {
-//       res.send(response.data);
-//     })
-//     .catch(function (error) {
-//       console.log(error);
-//     });
-// });
-
-/*
-// Reviews Service Amazon EC2 Instance
-// http://54.151.123.24:3002/items/1/
-app.get('/reviews', (req, res) => {
-  console.log('hitting seller endpoint');
-  axios.get(`http://${reviewsIP}/items/1/bundle.js`)
-    .then(function (response) {
-      res.send(response.data);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-});
-
-// Item Images Service Amazon EC2 Instance
-// http://13.52.213.118:3006/items/1/
-app.get('/images', (req, res) => {
-  console.log('hitting images endpoint');
-  axios.get(`http://${imagesIP}/items/1/bundle.js`)
-    .then(function (response) {
-      res.send(response.data);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-});
-
-// ##################################################
-// Rerouting requests from services to their correct ports if necessary
-// ##################################################
-
-// Shopping Service Amazon EC2 Instance
-// http://18.222.223.190:3004/items/1
-// Shopping Service
-app.get('/shopping/items/:itemId', (req, res) => {
-  let itemID = req.params.itemId;
-  axios.get(`http://${shoppingIP}/shopping/items/${itemID}`)
-    .then(function (response) {
-      res.send(response.data);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-});
-
-// Seller Service Amazon EC2 Instance
-// http://3.21.248.149:3005/items/2/
 // Seller Service
 app.get('/items/:item_id/seller', (req, res) => {
   console.log('getting to seller endpoint');
@@ -131,7 +109,7 @@ app.get('/shopping/items', (req, res) => {
 });
 
 app.get('/item/images', (req, res) => {
-  axios.get(`http://${imagesIP}/item/images`)
+  axios.get(`http://${imagesIP}/items/${itemID}/images`)
     .then(function (response) {
       res.send(response.data);
     })
@@ -140,12 +118,10 @@ app.get('/item/images', (req, res) => {
     });
 });
 
-// Item Images Service Amazon EC2 Instance
-// http://13.52.213.118:3006/items/1/
 // Images Service
 app.get('/item/:item_id/images', (req, res) => {
   let itemID = req.params.item_id;
-  axios.get(`http://${imagesIP}/item/${itemID}/images`)
+  axios.get(`http://${imagesIP}/items/${itemID}/images`)
     .then(function (response) {
       res.send(response.data);
     })
@@ -153,7 +129,6 @@ app.get('/item/:item_id/images', (req, res) => {
       console.log(error);
     });
 });
-*/
 
 app.listen(port, () => {
   console.log(`fetsyProxyServer listening at ${port}`);
